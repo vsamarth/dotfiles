55813c807647a853136bd19aac3bf36aa55d797b
Samarth Verma <samarthverma@icloud.com>
2023-12-23 17:25:46 +0800

    Enable treesitter for neovim

--- nvim/init.lua
+++ nvim/init.lua
@@ -111,6 +111,20 @@ require("lazy").setup({
     "nvim-tree/nvim-tree.lua",
     opts = {},
   },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup({
+        ensure_installed = { "c", "cpp", "lua", "go", "rust", "html", "typescript", "javascript", "bash" },
+        sync_install = false,
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end,
+  },
 })
 
 -- }}}
